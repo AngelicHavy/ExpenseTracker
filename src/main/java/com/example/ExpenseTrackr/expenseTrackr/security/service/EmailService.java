5ad13adbfd01bbbefe4dbcfc03e5f77aa40ebbd4
Anzhelika <aanzelikaa23@gmail.com>
2024-06-01 04:32:54 -0400

    add app

--- src/main/java/com/example/ExpenseTrackr/expenseTrackr/security/service/EmailService.java
+++ src/main/java/com/example/ExpenseTrackr/expenseTrackr/security/service/EmailService.java
@@ -11,27 +11,3 @@ import com.sendgrid.helpers.mail.objects.Email;
 
 import java.io.IOException;
 
-@Service
-public class EmailService {
-    @Value("${spring.sendgrid.api-key}")
-    private String sendGridApiKey;
-
-    public void sendVerificationEmail(String to, String code) throws Exception {
-        Email from = new Email("email");
-        String subject = "Verification Code";
-        Email toEmail = new Email(to);
-        Content content = new Content("text/plain", "Your verification code is: " + code);
-        Mail mail = new Mail(from, subject, toEmail, content);
-
-        SendGrid sg = new SendGrid(sendGridApiKey);
-        Request request = new Request();
-        try {
-            request.setMethod(Method.POST);
-            request.setEndpoint("mail/send");
-            request.setBody(mail.build());
-            sg.api(request);
-        } catch (IOException ex) {
-            throw new Exception("Error sending email", ex);
-        }
-    }
-}
